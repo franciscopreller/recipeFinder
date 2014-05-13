66fc25a8b8070fdbf3ad39c9070eee07ed9b2d1b
Francisco <francisco.preller@gmail.com>
2014-05-13 22:33:43 +1000

    add exceptions for container operators

---
+++ src/FP/RecipeFinderBundle/Exception/InvalidOperatorExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FP\RecipeFinderBundle\Exception;
+
+/**
+ * Interface for invalid operator exceptions for the Container class
+ */
+interface InvalidOperatorExceptionInterface
+{
+}
